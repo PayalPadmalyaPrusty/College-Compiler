859612b3e313e6140e45ceed6ceaa81d5af06a21
Payal Padmalaya Prusty <98264541+PayalPadmalyaPrusty@users.noreply.github.com>
2024-05-11 16:12:01 +0530

    lexer

---
+++ LEXER/test.cpp
@@ -0,0 +1,26 @@
+/*#include<iostream>
+#include<math.h>
+using namespace std;
+
+int main()
+{
+	int x,y,z;
+	cout<<"Enter a number and its power"<<endl;
+	cin>>x>>y;
+	z=pow(x,y);
+	cout<<"Your result is"<<z<<endl;
+	return 0;
+}*/
+#include<iostream>
+#include<math.h>
+using namespace std;
+
+int main(){
+int x,y;
+cout<<"Enter a number and power"<<endl;
+cin>>x>>y;
+int result;
+result=pow(x,y);
+cout<<"Your result is"<<result<<endl;
+return 0;
+}
